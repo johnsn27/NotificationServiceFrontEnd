03152ae3baf4d1dbc771124cc1b6799dccee77f6
Nathan Johnson <nathan.johnson@bbc.co.uk>
2019-01-15 09:42:12 +0000

    [SEP] Added igm dropdown dependency

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,14 +1,14 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Btn from '@bbc/igm-btn';
+import Dropdown from '@bbc/igm-dropdown-select'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-        <div className="buttonDiv">
+        <div className="    ">
           <Btn
             type="link"
             tab-index="1"
