487710593a28dddca4630e6a2e2cc6a5adf31082
Lana Allingham <lana.allingham@bbc.co.uk>
2019-01-18 17:24:57 +0000

    [TEST-FOUNDATIONS] Add cucumber-js to the project

--- src/__tests__/unit/App.test.js
+++ src/__tests__/unit/App.test.js
@@ -4,7 +4,15 @@ import { shallow, configure } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import App from '../../App';
 
-it('renders without crashing', () => {
+describe('App', () => {
   configure({ adapter: new Adapter() });
-  shallow(<App />);
+
+  it('renders without crashing', () => {
+    shallow(<App />);
+  });
+
+  it('contains a button', () => {
+  const app = shallow(<App/>);
+  expect(app.find('.Button').exists()).toBe(true);
+  });
 });
