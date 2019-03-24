2db428f3eeb4ff823c3f85e1a4b60e589dc899ca
selvad02 <dilani.selvanathan@bbc.co.uk>
2019-03-24 14:23:22 +0000

    Removed index file changes

--- src/index.js
+++ src/index.js
@@ -4,17 +4,12 @@ import { BrowserRouter } from 'react-router-dom';
 import './index.css';
 import * as serviceWorker from './serviceWorker';
 import Main from './Main/Main';
-import Login from './Login/Login';
-import Registration from './Registration/RegistrationButton';
 
 ReactDOM.render(
     <BrowserRouter>
         <Main />
     </BrowserRouter>, document.getElementById('root')
 );
-
-ReactDOM.render(<Login />, document.getElementById('root'));
-ReactDOM.render(<Registration />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
