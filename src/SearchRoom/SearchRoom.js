d77edf2cdfea924ea26e7e21f37a4810bfef8003
Lana Allingham <lana.allingham@bbc.co.uk>
2019-03-26 23:02:49 +0000

    Add asteriks to required fields and display errors (#31)

--- src/SearchRoom/SearchRoom.js
+++ src/SearchRoom/SearchRoom.js
@@ -4,7 +4,60 @@ import './SearchRoom.css';
 import Btn from '@bbc/igm-btn';
 
 class SearchRoom extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+          notification: {displayed: false, message: ''}
+        }
+    }
+    showError(message='') {
+        this.setState({notification: {displayed: true, message: message}})
+    }
+    search() {
+        const floor = document.getElementById('floor-textarea').value;
+        const roomName = document.getElementById('room-name-textarea').value;
+        const location = document.getElementById('location-textarea').value;
+        const date = document.getElementById('date').value && (new Date(document.getElementById('date').value).toISOString()).slice(0,10);
+        const startTime = document.getElementById('start-time').value;
+        const endTime = document.getElementById('end-time').value;
+        const capacity = document.getElementById('capacity-textarea').value;
+        if (date === '' || startTime === '' || endTime === '') {
+            this.showError(true, <>The following required fields are incomplete:
+            <ul>{date === '' && <li>Date</li>} {startTime === '' && <li>Start Time</li>} {endTime === '' && <li>End Time</li>}</ul></>);
+        } else if (isNaN(floor)) {
+            this.showError(`Floor '${floor}' is invalid`);
+        } else if ((capacity !== '' && capacity < 0) || isNaN(capacity)) {
+            this.showError(`Capacity '${capacity}' is invalid`);
+        } else {
+            try {
+                if (Date.parse(`${date} ${startTime}:00`) > Date.parse(`${date} ${endTime}:00`)) {
+                    this.showError('End Time cannot be before Start Time');
+                    return;
+                }
+            } catch (e) {
+                this.showError(true, e);
+            }
+            window.location.href = (
+                `/meeting-rooms?capacity=${
+                    capacity
+                }${
+                    roomName  && `&name=${roomName.replace(' ', '%20')}`
+                }${
+                    location && `&location=${location}`
+                }${
+                    floor && `&floor=${floor}`
+                }${
+                    startTime && `&start=${`${date}%20${startTime}:00`}`
+                }${
+                    endTime && `&end=${`${date}%20${endTime}:00`}`
+                }${
+                    `&show_unavailable=${document.getElementById('unavailable-checkbox').checked}`
+                }`
+            )
+        } 
+    }
     render() {
+        const { notification } = this.state;
         return (
             <div>
                 <div className="search-room-title">
@@ -12,6 +65,7 @@ class SearchRoom extends Component {
                 </div>
                 <div className="search-room-contents">
                     <div className="search-room-contents-container">
+                        <div role="alert" hidden={!notification.displayed} className="notification"><strong></strong>{notification.message}</div>
                         <div className="textbox">
                             <div className="label" id="room-name-label">
                                 <p>Room Name:</p>
@@ -42,19 +96,19 @@ class SearchRoom extends Component {
                         </div>
                         <div className="textbox">
                             <div className="label">
-                                <p>Date:</p>
+                                <p>Date<strong>*</strong>:</p>
                             </div>
                             <input type="date" className="textarea" rows="2" id="date" placeholder="dd/mm/yyyy"/>
                         </div>
                         <div className="textbox">
                             <div className="label">
-                                <p>Start Time:</p>
+                                <p>Start Time<strong>*</strong>:</p>
                             </div>
                             <input type="time" className="textarea" rows="2" id="start-time" placeholder="hh:mm"/>
                         </div>
                         <div className="textbox">
                             <div className="label">
-                                <p>End Time:</p>
+                                <p>End Time<strong>*</strong>:</p>
                             </div>
                             <input type="time" className="textarea" rows="2" id="end-time" placeholder="hh:mm"/>
                         </div>
@@ -68,31 +122,7 @@ class SearchRoom extends Component {
                                     type="primary"
                                     tab-index="1"
                                     className="Button"
-                                    onClick={() => {
-                                        const floor = document.getElementById('floor-textarea').value;
-                                        const roomName = document.getElementById('room-name-textarea').value;
-                                        const location = document.getElementById('location-textarea').value;
-                                        const date = document.getElementById('date').value && (new Date(document.getElementById('date').value).toISOString()).slice(0,10);
-                                        const startTime = document.getElementById('start-time').value;
-                                        const endTime = document.getElementById('end-time').value;
-                                        window.location.href = (
-                                                `/meeting-rooms?capacity=${
-                                                    document.getElementById('capacity-textarea').value
-                                                }${
-                                                    roomName  && `&name=${roomName.replace(' ', '%20')}`
-                                                }${
-                                                    location && `&location=${location}`
-                                                }${
-                                                    floor && `&floor=${floor}`
-                                                }${
-                                                    startTime && `&start=${`${date}%20${startTime}:00`}`
-                                                }${
-                                                    endTime && `&end=${`${date}%20${endTime}:00`}`
-                                                }${
-                                                    `&show_unavailable=${document.getElementById('unavailable-checkbox').checked}`
-                                                }`
-                                            )
-                                    }}
+                                    onClick={() => this.search()}
                                 >
                                     <span>Search</span>
                                 </Btn>
