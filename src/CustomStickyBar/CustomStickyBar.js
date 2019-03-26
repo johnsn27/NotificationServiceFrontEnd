4d02f41b2929b51e4250f7368f9cf2dee2179f64
Nathan Johnson <nathan.johnson@bbc.co.uk>
2019-03-26 21:54:11 +0000

    Logout Button (#30)
    * Completed logout button
    
    * Moved logout button

--- src/CustomStickyBar/CustomStickyBar.js
+++ src/CustomStickyBar/CustomStickyBar.js
@@ -3,12 +3,22 @@ import PropTypes from 'prop-types';
 import StickyBar from '@bbc/igm-sticky-bar'
 import './CustomStickyBar.css'
 import '@bbc/igm-sticky-bar/dist/StickyBar.css';
+import Btn from '@bbc/igm-btn'
 
 function CustomStickyBar({ id, className, text }) {
   return (
     <StickyBar id={id} className={className}>
-      <h1>{text}</h1>
-    </StickyBar>
+      <div id="container" className="container">
+        <div id="text" className="text">
+          <h1>{text}</h1>
+        </div>
+        <div id="logout" className="logout">
+          <Btn className="logout-button" onClick={() => { window.location.href = '/' }}>
+            Logout
+        </Btn>
+        </div>
+      </div>
+    </StickyBar >
   );
 }
 
